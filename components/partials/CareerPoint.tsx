4a96fd0da7aec78e3567aae5e62c3cfb34f8304c
Jerome Haas <j.haas@cyberplanet.ch>
2021-02-14 19:25:06 +0100

    Implement: work section

---
+++ components/partials/CareerPoint.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from '@/styles/CareerPoint.module.scss';
+
+interface Props {
+  jobDetais: any;
+}
+
+const CareerPoint: React.FunctionComponent<Props> = ({ jobDetails }) => {
+
+  return (
+    <div className={styles.wrapper}>
+      <h3>{jobDetails.title}</h3>
+      <p>{jobDetails.metaData}</p>
+      <p>{jobDetails.duration}</p>
+      <p>{jobDetails.description}</p>
+    </div>
+  );
+
+}
+
+export default CareerPoint;
--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,64 @@ const codepens= [
   {
     link: 'https://www.npmjs.com/package/github-cube',
     name: 'github-cube',
-    date: '09.2020'
+    date: '09.2020',
+  },
+];
+
+const jobs = [
+  {
+    title: 'Codeworks Bootcamp',
+    metaData: 'Codeworks | Spain, Barcelona',
+    duration: 'October 2020 - Today',
+    description: 'Codeworks is an immersive coding bootcamp focusing on JavaScript and web development. In 12 weeks of full-time training and over 1000 hours of coding and three projects, I deepened my knowledge in JavaScript, React, TypeScript, NodeJS, Express and many other technologies and frameworks.'
+  },
+  {
+    title: 'Frontend Developer',
+    metaData: 'Arcmedia AG | Switzerland, Lucerne',
+    duration: 'December 2018 - October 2020',
+    description: 'Arcmedia is a web agency with a focus on e-commerce and email marketing. During almost two years I worked as a frontend developer in the email marketing team and was responsible for developing forms with HTML, CSS and JavaScript for well-known clients. On top of that, I developed professional HTML email templates for our clients. I was also responsible for our IT infrastructure as a system administrator, managing all accounts and hardware for the company.'
+  },
+  {
+    title: 'Self Study Web Development',
+    metaData: 'Arcmedia AG | Switzerland, Lucerne',
+    duration: 'DDecember 2017 - December 2018',
+    description: 'During my travels through Europe and South-East-Asia I decided to become a web developer. I took some time off and learned about HTML, CSS and JavaScript and had my first experience with InDesign, Illustrator, Photoshop and After Effects. During this time I worked as a receptionist at Roche to have a stable income.'
+  },
+  {
+    title: 'Backpacking',
+    metaData: 'Europe and Southeast Asia',
+    duration: 'April 2017 - December 2017',
+    description: 'After an intense period of service in Kosovo, I took time off to travel Europe and Southeast Asia to process all the impressions and gather new ones. During this time I backpacked in Portugal, Spain, Italy, Switzerland, France, La Reunion, Sri Lanka, Thailand, Vietnam, the Philippines, Singapore and Indonesia.'
+  },
+  {
+    title: 'Transmission Specialist, Seputy IT Officer, Military Assistant',
+    metaData: 'Swissint | Kosovo, Pristina',
+    duration: 'June 2015 - April 2017',
+    description: 'International peace support is one of the three tasks of the Swiss Armed Forces laid down in the federal constitution and by the federal law on the swiss armed forces. Swissint is responsible for the conduct of peace support operations. After an intensive 3-month training from Swissint, I served two years in the KFOR forces. During this time, I worked in the tactical operations center and was responsible for the coordination and authorization of our mobile units outside the protected camp. In addition, as a military assistant I was responsible for coordinating the agenda of our national contingent commander and planning and executing relocations, as well as providing personal protection.'
+  },
+  {
+    title: 'Ventilation System Builder',
+    metaData: 'Jakob Haas AG | Switzerland, Merenschwand',
+    duration: 'June 2014 - June 2015',
+    description: 'In our family business, I took responsibility for various areas. I was responsible for procuring materials and controlling their delivery, providing and maintaining IT, and installing simple ventilation systems.'
+  },
+  {
+    title: 'Salesman',
+    metaData: 'Magazine zum Globus AG | Switzerland, Lucerne',
+    duration: 'August 2013 - June 2014',
+    description: 'At Globus, I worked as a salesman and was responsible for the business area. I advised customers interested in buying suits, ties, shoes and other clothing.'
+  },
+  {
+    title: 'Infantryman',
+    metaData: 'Swiss Army | Switzerland, Yverdon les baines',
+    duration: 'March 2013 - August 2013',
+    description: 'I fulfilled the obligation of my country and completed a training as an infantryman and was trained in urban warfare, navigation and radio engineering.'
+  },
+  {
+    title: 'Salesman Apprenticeship',
+    metaData: 'Magazine to Globus AG | Switzerland, Lucerne',
+    duration: 'August 2009 - March 2013',
+    description: 'After school, I started my career as a salesman at Globus. At this time I was very interested in observing body language and communication. During the training I learned how to communicate, how to mirror body language and the basics of good manners.'
   },
 ]
 
@@ -108,14 +165,11 @@ export default function Home() {
           <Dropdown name={"Websites"}  items={websites} status={websiteDropdownStatus} open={() => setWebsiteDropdownStatus(!websiteDropdownStatus)}/>
           <Dropdown name={"NPM Packages"} items={npmPackages} status={npmPackageDropdownStatus} open={() => setNpmPackageDropdownStatus(!npmPackageDropdownStatus)} />
           <Dropdown name={"Codepens"} items={codepens} status={codepenDropdownStatus} open={() => setCodepenDropdownStatus(!codepenDropdownStatus)} />
-
           </Section>
 
-        // --------------------------------------------- //
-        // CAREER
-        // --------------------------------------------- //
         <Section id={"career"}>
           <h2>Career</h2>
+          {jobs.map((job, index) =>  <CareerPoint jobDetails={job} /> )}
         </Section>
 
         <Section id={"tools"}>
